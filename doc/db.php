892b1bb8d29083f4e667ac5e393b4cecc6702bc1
lifu.guo <631929063@qq.com>
2018-12-04 15:50:48 +0800

    2018/12/04-15:50:00

---
+++ doc/db.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => '127.0.0.1',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'pigcms',
+  'DB_USER' => 'root',
+  'DB_PWD' => 'root',
+  'DB_PREFIX' => 'tp_',
+  'agent_version' => 1,
+);
+?>
